6e4f9685e48020c82c40b7d5219dc7ea41c73009
김나라 <142205401+C5D2@users.noreply.github.com>
2024-10-02 11:10:23 +0900

    [level 1] Title: 폰켓몬, Time: 40.98 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,5 @@
 function solution(nums) {
     const getNum = Math.floor(nums.length / 2);
     const newArray = nums.filter((v, i) => { return nums.indexOf(v) === i});
-    result = getNum <= newArray.length ? getNum : newArray.length;
-    return result;
+    return getNum <= newArray.length ? getNum : newArray.length;
 }
