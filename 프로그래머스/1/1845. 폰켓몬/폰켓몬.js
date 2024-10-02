7c5704e733eebb014ff4586894be34f6a78750f4
김나라 <142205401+C5D2@users.noreply.github.com>
2024-10-02 11:14:04 +0900

    [level 1] Title: 폰켓몬, Time: 1.30 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,13 @@
+// function solution(nums) {
+//     const getNum = Math.floor(nums.length / 2);
+//     const newArray = nums.filter((v, i) => { return nums.indexOf(v) === i});
+//     return getNum <= newArray.length ? getNum : newArray.length;
+// }
+
 function solution(nums) {
-    const getNum = Math.floor(nums.length / 2);
-    const newArray = nums.filter((v, i) => { return nums.indexOf(v) === i});
-    return getNum <= newArray.length ? getNum : newArray.length;
+    const uniqueTypes = new Set(nums); // 중복을 제거하여 폰켓몬 종류 수를 구함
+    const maxSelection = nums.length / 2; // 선택할 수 있는 최대 수
+
+    // 최대 선택 가능한 폰켓몬 수와 실제 종류 수 중 최소값을 반환
+    return Math.min(uniqueTypes.size, maxSelection);
 }
