f73b8de44834050d8d74232f1a1b4009718027d5
김나라 <142205401+C5D2@users.noreply.github.com>
2024-10-04 18:41:10 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    let answer = [];
+    for(let command of commands){
+        const [i, j, k] = command;
+        const sliceArr = array.slice(i - 1, j);
+        const sortedSliceArr = sliceArr.sort((a,b) => a - b);
+        answer.push(sortedSliceArr[k-1]);
+    }
+    return answer;
+}
