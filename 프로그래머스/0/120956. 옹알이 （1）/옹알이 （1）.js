8a25e45d4c59d803d83fd6b0516904d8cdd05daa
김나라 <142205401+C5D2@users.noreply.github.com>
2024-10-09 19:53:27 +0900

    [level 0] Title: 옹알이 (1), Time: 0.46 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,19 +1,32 @@
+// function solution(babbling) {
+//     let voca = ["aya", "ye", "woo", "ma"];
+//     let possibleVoca = [...voca];
+//     for(let i = 0; i < voca.length; i++) {
+//         for(let j = 0; j < voca.length; j++)
+//             if(i !== j){
+//             possibleVoca.push(voca[i] + voca[j]);
+//                 for(let k = 0; k < voca.length; k++){                    
+//                     if(i !== k && j !== k){
+//                     possibleVoca.push(voca[i] + voca[j] + voca[k]);
+//                 for(let l = 0; l < voca.length; l++){
+//                     if(i !== l && j !== l && k !== l){
+//                     possibleVoca.push(voca[i] + voca[j] + voca[k] + voca[l]);
+//                     }
+//                 }
+//     }}}};
+//     const answer = babbling.filter((value) => possibleVoca.includes(value));
+//     return answer.length;
+// }
+
 function solution(babbling) {
-    let voca = ["aya", "ye", "woo", "ma"];
-    let possibleVoca = [...voca];
-    for(let i = 0; i < voca.length; i++) {
-        for(let j = 0; j < voca.length; j++)
-            if(i !== j){
-            possibleVoca.push(voca[i] + voca[j]);
-                for(let k = 0; k < voca.length; k++){                    
-                    if(i !== k && j !== k){
-                    possibleVoca.push(voca[i] + voca[j] + voca[k]);
-                for(let l = 0; l < voca.length; l++){
-                    if(i !== l && j !== l && k !== l){
-                    possibleVoca.push(voca[i] + voca[j] + voca[k] + voca[l]);
-                    }
-                }
-    }}}};
-    const answer = babbling.filter((value) => possibleVoca.includes(value));
-    return answer.length;
+    const voca = ["aya", "ye", "woo", "ma"];
+    
+    return babbling.filter(word => {
+        let temp = word;
+        for (let v of voca) {
+            if (temp.includes(v.repeat(2))) return false; 
+            temp = temp.split(v).join(' ');
+        }
+        return temp.trim().length === 0;
+    }).length;
 }
