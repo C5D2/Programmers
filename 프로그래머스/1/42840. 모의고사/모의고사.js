fda9da8554d643aa9ce21ca617777e5ec92d60be
김나라 <142205401+C5D2@users.noreply.github.com>
2024-10-12 22:05:31 +0900

    [level 1] Title: 모의고사, Time: 1.88 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,17 @@
+function solution(answers) {
+    const patterns = [ 
+        [1, 2, 3, 4, 5], 
+        [2, 1, 2, 3, 2, 4, 2, 5], 
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ];
+    const getScore = (pattern) => answers.reduce((score, answer, index) => 
+        score + (answer === pattern[index % pattern.length] ? 1 : 0), 0
+    );
+    
+    const scores = patterns.map(getScore);
+    const maxScore = Math.max(...scores);
+    
+    const result = scores.map((score, index) => score === maxScore ? index + 1 : null).filter((num) => num !== null);
+    
+    return result.sort((a, b) => a - b);
+}
