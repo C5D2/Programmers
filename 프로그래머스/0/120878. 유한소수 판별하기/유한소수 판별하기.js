c51cd8df79c86bf249daa2ade7d7a455d773d63f
김나라 <142205401+C5D2@users.noreply.github.com>
2024-10-19 23:51:31 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,15 +1,16 @@
 function solution(a, b) {
-    let gcd = 1;
+    // let gcd = 1;
     const divisor = [];
     const prime_number = [];
     
-    for (let i = 2; i <= Math.min(a, b); i++) {
-            if(a % i === 0 && b % i === 0) {
-                gcd = i;
-            }
-        }
+//     for (let i = 2; i <= Math.min(a, b); i++) {
+//             if(a % i === 0 && b % i === 0) {
+//                 gcd = i;
+//             }
+//         }
+    const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
     
-    const RD = b/gcd;
+    const RD = b/gcd(a, b);
 
     for(let j = 1; j <= RD; j++) {
         if (RD % j === 0) divisor.push(j)
