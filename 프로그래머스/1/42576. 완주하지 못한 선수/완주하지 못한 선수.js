1f4a47aaebd7a85a216e63058a83ba664481241c
김나라 <142205401+C5D2@users.noreply.github.com>
2024-10-29 20:55:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 109.66 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,21 @@
+// function solution(participant, completion) {
+//     const allArray = [...participant, ...completion];
+//     const nameCounts = allArray.reduce((acc, name) => {
+//         acc[name]=(acc[name] || 0) + 1;
+//         return acc;}, {});
+//     const answer = Object.keys(nameCounts).filter(name=>nameCounts[name] % 2 !== 0);
+//     return answer.toString();
+// }
+
 function solution(participant, completion) {
-    const allArray = [...participant, ...completion];
-    const nameCounts = allArray.reduce((acc, name) => {
-        acc[name]=(acc[name] || 0) + 1;
-        return acc;}, {});
-    const answer = Object.keys(nameCounts).filter(name=>nameCounts[name] % 2 !== 0);
-    return answer.toString();
+    var answer = '';
+    participant.sort();
+    completion.sort();
+    for(var i = 0 ; i < participant.length; i++){
+        if(participant[i] !== completion[i]){
+            answer = participant[i];
+            break;
+        }
+    }
+    return answer;
 }
