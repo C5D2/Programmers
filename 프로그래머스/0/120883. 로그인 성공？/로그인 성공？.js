85ed337289aa706e74533e7da81b32630b168909
김나라 <142205401+C5D2@users.noreply.github.com>
2024-11-02 02:45:53 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,6 +1,6 @@
 function solution(id_pw, db) {
     const [id, pw] = id_pw;
-    for(let [dbId, dbPw] of db) {
+    for(const [dbId, dbPw] of db) {
         if (dbId === id) {
             return dbPw === pw ? "login" : "wrong pw"
         }
