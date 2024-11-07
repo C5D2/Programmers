aecbce626b0540fda7b0e4fa5e6273ef459fd065
김나라 <142205401+C5D2@users.noreply.github.com>
2024-11-07 22:21:15 +0900

    [level 0] Title: 369게임, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,10 +1,12 @@
 function solution(order) {
-    let answer = 0;
-    let arr = [...String(order)];
-    for(let i of arr) {
-        if ( i === '3' || i === '6' || i === '9') {
-            answer += 1;
-        }
-    }
-    return answer;
+    // let answer = 0;
+    // let arr = [...String(order)];
+    // for(let i of arr) {
+    //     if ( i === '3' || i === '6' || i === '9') {
+    //         answer += 1;
+    //     }
+    // }
+    // return answer;
+    const answer = order.toString().match(/[369]/g) ?? [];
+    return answer.length;
 }
