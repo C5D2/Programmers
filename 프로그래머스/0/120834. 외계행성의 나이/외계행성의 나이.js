702286723d4191243bf1fbd182f81a8c0ea24f52
김나라 <142205401+C5D2@users.noreply.github.com>
2024-11-07 21:05:06 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,16 +1,18 @@
 function solution(age) {
-      let obj = {
-        0 : "a",
-        1 : "b",
-        2 : "c",
-        3 : "d",
-        4 : "e",
-        5 : "f",
-        6 : "g",
-        7 : "h",
-        8 : "i",
-        9 : "j"
-    }
-    let age962 = age + "";
-    return age962.split("").map((element) => obj[element]).join("");
+    //   let obj = {
+    //     0 : "a",
+    //     1 : "b",
+    //     2 : "c",
+    //     3 : "d",
+    //     4 : "e",
+    //     5 : "f",
+    //     6 : "g",
+    //     7 : "h",
+    //     8 : "i",
+    //     9 : "j"
+    // }
+    // let age962 = age + "";
+    // return age962.split("").map((element) => obj[element]).join("");
+    const alpha = "abcdefghij";
+    return Array.from(age.toString()).map((v) => alpha[v]).join("")
 }
