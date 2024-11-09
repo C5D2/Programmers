c8ea309219ca57f5b1bcae2dd338e61e775b9960
김나라 <142205401+C5D2@users.noreply.github.com>
2024-11-09 20:42:50 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.13 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,4 +1,5 @@
 function solution(s) {
     var answer = '';
-    return [...s].filter((v, i) => s.indexOf(v) === s.lastIndexOf(v)).sort().join('');
+    // return [...s].filter((v, i) => s.indexOf(v) === s.lastIndexOf(v)).sort().join('');
+    return [...s].filter((c) => s.split(c).length === 2).sort().join('');
 }
