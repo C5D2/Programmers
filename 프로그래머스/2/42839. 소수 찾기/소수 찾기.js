e6a9a8e0592da10924c18718ee888b3502611ffc
김나라 <142205401+C5D2@users.noreply.github.com>
2024-12-18 22:14:57 +0900

    [level 2] Title: 소수 찾기, Time: 3.60 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,30 +1,4 @@
 function solution(numbers) {
     const numbersArr = numbers.split('')
-    const visited = new Array(numbers.length).fill(false);
-    const result = new Set();
-    
-    const combine = (str) => {
-        if (str.length > 0) {
-            result.add(Number(str));
-        }
-        
-        for (let i = 0; i < numbersArr.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                combine(str + numbersArr[i]);
-                visited[i] = false;
-            }
-        }
-    }
-    combine('');
-
-    const isPrime = (num) => {
-        if (num < 2) return false;
-        for (let i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i === 0) return false;
-        }
-        return true;
-    }
-    
-    return [...result].filter(isPrime).length;
+    return numbersArr;
 }
