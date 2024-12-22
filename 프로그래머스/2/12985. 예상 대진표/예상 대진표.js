90a13348ced50c39cef08a0d351230d7b5dfc98c
김나라 <142205401+C5D2@users.noreply.github.com>
2024-12-22 22:34:12 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -2,9 +2,11 @@ function solution(n,a,b)
 {
     let round = 0;
     
-    for(; a !== b; round++){
+    // for(; a !== b; round++) {
+    while(a !== b) {
         a = Math.ceil(a / 2);
         b = Math.ceil(b / 2);
+        round ++;
     }
     return round;
 }
