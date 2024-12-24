ac08dadc3965e3483d3e42436d06899fcd2f9e0d
김나라 <142205401+C5D2@users.noreply.github.com>
2024-12-25 01:49:47 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,7 +1,8 @@
 function solution(phone_number) {
-    const answer = [...phone_number];
-    for(let i = 0; i < answer.length - 4; i++) {
-        answer[i] = "*"
-    }
-    return answer.join('');
+    // const answer = [...phone_number];
+    // for(let i = 0; i < answer.length - 4; i++) {
+    //     answer[i] = "*"
+    // }
+    // return answer.join('');
+    return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
 }
