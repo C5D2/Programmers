4bf4874e028f4532ae6360e285a7b8ac1cc2beb0
김나라 <142205401+C5D2@users.noreply.github.com>
2025-01-01 22:18:45 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,10 +1,16 @@
 function solution(n) {
-    let map = new Map();
-    for(let i = 1; i <= n; i++) {
+    // let map = new Map();
+    // for(let i = 1; i <= n; i++) {
+    //     if(n % i === 0) {
+    //         map.set(i, n / i)
+    //     }
+    // }
+    // return map.size;
+    let count = 0;
+    for(let i = 1; i <= Math.sqrt(n); i++) {
         if(n % i === 0) {
-            map.set(i, n / i)
+            count += n / i === i ? 1 : 2;
         }
-        console.log(map)
     }
-    return map;
+    return count;
 }
