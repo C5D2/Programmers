c75bfab88f9055121e76958796bd452eaf529f3a
김나라 <142205401+C5D2@users.noreply.github.com>
2025-01-11 19:50:30 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,12 +1,13 @@
 function solution(n_str) {
-    var answer = n_str;
-    for(let i = 0; i < n_str.length; i++) {
-        if(n_str[i] == 0) {
-            answer = n_str.slice(i + 1);
-        }
-        else if(n_str[i] !== 0) {
-            break;
-        }
-    }
-    return answer;
+    // let answer = n_str;
+    // for(let i = 0; i < n_str.length; i++) {
+    //     if(n_str[i] == 0) {
+    //         answer = n_str.slice(i + 1);
+    //     }
+    //     else if(n_str[i] !== 0) {
+    //         break;
+    //     }
+    // }
+    // return answer;
+    return String(Number(n_str));
 }
